a004938305e798b896b0ed132d6672a486d4b1ed
imelnath <fiomelnathany@gmail.com>
2023-09-13 15:46:12 +0700

    feat: create class

---
+++ Berkati-Backend/Models/Admin.cs
@@ -0,0 +1,22 @@
+﻿namespace Berkati_Backend.Models
+{
+    public class Admin
+    {
+        private string _id;
+        private string username;
+        private string password;
+        private DateTime lastLogin;
+        private bool isSuperUser;
+
+        public string Id { get => _id; set => _id = value; }
+        public string Username { get => username; set => username = value; }
+        public string Password { get=> password; set => password = value; }
+        public DateTime LastLogin { get => lastLogin; set => lastLogin = value; }
+        public bool IsSuperUser { get => isSuperUser; set => isSuperUser = value; }
+
+        public Admin()
+        {
+        
+        }
+    }
+}
---
+++ Berkati-Backend/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Berkati_Backend.Models
+{
+    public class User
+    {
+        private string _id;
+        private string nama;
+        private string telp;
+
+        public string Id { get => _id; set => _id = value; }
+        public string Nama { get => nama; set => nama = value; }
+        public string Telp { get => telp; set => telp = value; }
+
+        public User() { }
+    }
+}
