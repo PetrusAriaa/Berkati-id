b323d4aff536a063c56177e655795d0c2037ee3a
KhairunNisaZ <khairun.nisa.zuqri92@gmail.com>
2023-10-26 11:05:52 +0700

    feat: create manage admin and manage partner page

--- Berkati-Frontend/Pages/Page3.xaml.cs
+++ Berkati-Frontend/Pages/Page3.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,75 @@ namespace Berkati_Frontend.Pages
     /// </summary>
     public partial class Page3 : Page
     {
+        // Daftar objek untuk menampung data
+        private ObservableCollection<UserData> userDataList;
+        private UserData selectedUser; // Deklarasikan variabel untuk menyimpan item yang dipilih
+
         public Page3()
         {
             InitializeComponent();
+            // Inisialisasi daftar data
+            userDataList = new ObservableCollection<UserData>();
+            DataGrid.ItemsSource = userDataList;
+        }
+
+        private void AddAdminBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Mendapatkan nilai dari input
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            // Menambahkan data ke daftar
+            userDataList.Add(new UserData { Username = username, Password = password });
+
+
+            // Reset TextBoxes setelah menambahkan admin
+            UsernameTextBox.Clear();
+            PasswordBox.Clear();
+        }
+
+        private void DeleteAdminBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataGrid.SelectedItem != null)
+            {
+                // Mengambil item yang dipilih dari DataGrid
+                UserData selectedUser = (UserData)DataGrid.SelectedItem;
+
+                // Menghapus item yang dipilih dari daftar
+                userDataList.Remove(selectedUser);
+
+                // Reset TextBoxes setelah menghapus admin
+                UsernameTextBox.Clear();
+                PasswordBox.Clear();
+            }
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DataGrid.SelectedItem != null)
+            {
+                // Mengambil item yang dipilih dari DataGrid
+                UserData selectedUser = (UserData)DataGrid.SelectedItem;
+
+                // Menampilkan nilai item yang dipilih di dalam inputan
+                UsernameTextBox.Text = selectedUser.Username;
+                PasswordBox.Password = selectedUser.Password;
+
+                // Mengatur DataGrid ke mode baca saja
+                DataGrid.IsReadOnly = true;
+            }
+            else
+            {
+                // Mengatur DataGrid ke mode aktif
+                DataGrid.IsReadOnly = false;
+            }
+        }
+
+        // Kelas untuk data pengguna
+        public class UserData
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
         }
     }
 }
