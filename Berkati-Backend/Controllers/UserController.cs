f0d94ee9c783864d841fe5b8565c51c4da59436c
imelnath <fiomelnathany@gmail.com>
2023-10-26 22:42:58 +0700

    feat: update user and requests api

--- Berkati-Backend/Controllers/UserController.cs
+++ Berkati-Backend/Controllers/UserController.cs
@@ -34,7 +34,50 @@ namespace Berkati_Backend.Controllers
             {
                 return StatusCode(500, ex.Message);
             }
-            
+        }
+
+        [HttpGet("requests")]
+        public IActionResult GetUserRequest()
+        {
+            try
+            {
+                List<User> listUser = users.GetAllUser();
+                var _data = listUser
+                    .SelectMany(user => user.Requests
+                        .Select(request =>
+                            new
+                            {
+                                userId = user.Id,
+                                nama = user.Nama,
+                                telp = user.Telp,
+                                requestId = request.Id,
+                                tanggal = request.Tanggal,
+                                jalan = request.Jalan,
+                                rt = request.Rt,
+                                rw = request.Rw,
+                                kel = request.Kel,
+                                kec = request.Kec,
+                                kab_kota = request.Kab_kota,
+                                provinsi = request.Provinsi,
+                                kodepos = request.Kodepos,
+                                est_jumlah = request.Est_jumlah,
+                                status = request.Status
+                            }
+                        )
+                    ).ToList();
+
+                var res = new
+                {
+                    data = _data,
+                    length = _data.Count,
+                    accessedAt = DateTime.UtcNow
+                };
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpPost]
